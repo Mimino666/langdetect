3bd112da39b988477d43fe17e7afe640a007712b
Michal Danilak <mimino.coder@gmail.com>
2014-05-17 02:15:04 +0200

    Moved factory intialization into detector_factory module.

--- langdetect/detector_factory.py
+++ langdetect/detector_factory.py
@@ -107,3 +107,26 @@ class DetectorFactory(object):
 
     def get_lang_list(self):
         return list(self.langlist)
+
+
+PROFILES_DIRECTORY = path.join(path.dirname(__file__), 'profiles')
+_factory = None
+
+def init_factory():
+    global _factory
+    if _factory is None:
+        _factory = DetectorFactory()
+        _factory.load_profile(PROFILES_DIRECTORY)
+
+def detect(text):
+    init_factory()
+    detector = _factory.create()
+    detector.append(text)
+    return detector.detect()
+
+
+def detect_langs(text):
+    init_factory()
+    detector = _factory.create()
+    detector.append(text)
+    return detector.get_probabilities()
