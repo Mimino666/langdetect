72c6c6cb5a59faeb92f34b6d0336198b895c210c
Michal Danilak <mimino.coder@gmail.com>
2016-04-03 20:07:36 +0200

    Upgraded to version 1.0.6.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open('LICENSE') as f:
 
 setup(
     name='langdetect',
-    version='1.0.5',
+    version='1.0.6',
     description='Language detection library ported from Google\'s language-detection.',
     long_description=readme,
     author='Michal Mimino Danilak',
