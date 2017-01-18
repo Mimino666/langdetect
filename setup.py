204ae7206be5dc847e7faba495c7fd2fcf91005c
Hugo <hugovk@users.noreply.github.com>
2017-01-18 11:33:44 +0200

    Python 3.6 is out

--- setup.py
+++ setup.py
@@ -34,5 +34,8 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ]
 )
